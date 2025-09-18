0932efa11b911d864559745817cd7147d9ea24b8
DorakuZz <santiago.pjz@hotmail.com>
2025-09-17 21:59:58 -0500

    Información personal

---
+++ resources/views/layouts/information.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>MI NOMBRE ES SANTIAGO PÉREZ</h1>
+    <br>
+    <h3>Fecha de nacimiento: 23 de abril del 2006</h3>
+    <br>
+    <h3>Lugar de nacimiento: Aguachica, Cesar</h3>
+    <br>
+    <h3>Nombre de el padre: José Luis</h3>
+    <br>
+    <h3>Nombre de la madre: Carolina</h3>
+    <br>
+    <h3>Nombre de la hermana: María Clara</h3>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/nombre', function() {
-    return view('layouts.nombre');
+Route::get('/information', function() {
+    return view('layouts.information');
 });
